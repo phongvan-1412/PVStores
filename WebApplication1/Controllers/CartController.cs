46f3826383409520dc34f27c9d9cd417563f3d62
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2023-03-22 01:18:15 +0700

    -Integrate Stripe -Check data when checkout (success)

--- WebApplication1/Controllers/CartController.cs
+++ WebApplication1/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 using WebApplication1.Utilities;
 using WebApplication1.ViewModels;
 
@@ -15,6 +16,51 @@ namespace WebApplication1.Controllers
                 lstBillDetailView = new List<BillDetailViewModels>();
             }
             return View(lstBillDetailView);
+        }
+
+        [HttpPost]
+        public IActionResult CheckOut(string stripeEmail, string stripeToken)
+        {
+            List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
+            if (lstBillDetailView == null)
+            {
+                lstBillDetailView = new List<BillDetailViewModels>();
+            }
+
+            var customers = new CustomerService();
+            var charges = new ChargeService();
+
+            var customer = customers.Create(new CustomerCreateOptions
+            {
+                Email = stripeEmail,
+                Source = stripeToken
+            });
+
+            var charge = charges.Create(new ChargeCreateOptions
+            {
+                Amount = long.Parse(lstBillDetailView.Sum(p => p.Total).ToString()) * 100,
+                Description = "Test Payment",
+                Currency = "usd",
+                Customer = customer.Id,
+                //ReceiptEmail = stripeEmail
+                //Metadata = new Dictionary<string, string>
+                //{
+                //    {"OrderID", "1"},
+                //    {"ReceiptNo", "1"}
+                //}
+            });
+
+            if (charge.Status.Equals("Succeeded"))
+            {
+                string BalanceTransactionID = charge.BalanceTransactionId;
+                return View();
+            }
+            else
+            {
+
+            }
+
+            return View();
         }
     }
 }
---
+++ WebApplication1/Utilities/StripeSettings.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication1.Models.entities;
+
+namespace WebApplication1.Utilities
+{
+    public class StripeSettings
+    {
+        public string Secretkey { get; set; }
+        public string Publishablekey { get; set; }
+    }
+}
