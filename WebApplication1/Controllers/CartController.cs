5377430bc933fe53aec037c1d7f5339fb3ab591d
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2023-04-06 20:43:51 +0700

    -Ajax method for increase + decrease product quantity

--- WebApplication1/Controllers/CartController.cs
+++ WebApplication1/Controllers/CartController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
 using Stripe;
+using System.Text.Json;
 using WebApplication1.Utilities;
 using WebApplication1.ViewModels;
+using System.Collections.Generic;
 
 namespace WebApplication1.Controllers
 {
@@ -15,7 +18,116 @@ namespace WebApplication1.Controllers
             {
                 lstBillDetailView = new List<BillDetailViewModels>();
             }
+
             return View(lstBillDetailView);
+        }
+
+        //public IActionResult Increase(int id)
+        //{
+        //    List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
+        //    if (lstBillDetailView == null)
+        //    {
+        //        lstBillDetailView = new List<BillDetailViewModels>();
+        //    }
+
+        //    if (lstBillDetailView.Any(p => p.ProductID == id))
+        //    {
+        //        decimal productPrice = lstBillDetailView.Where(p => p.ProductID == id).FirstOrDefault().Price;
+        //        int updateQuantity = lstBillDetailView.Where(p => p.ProductID == id).FirstOrDefault().Quantity;
+
+        //        if (updateQuantity > 19) 
+        //        {
+        //            updateQuantity = lstBillDetailView.Where(p => p.ProductID == id).FirstOrDefault().Quantity;
+        //        }
+        //        else
+        //        {
+        //            updateQuantity = lstBillDetailView.Where(p => p.ProductID == id).FirstOrDefault().Quantity += 1;
+        //        }
+
+        //        decimal totalPrice = lstBillDetailView.Where(p => p.ProductID == id).FirstOrDefault().Total = updateQuantity * productPrice;
+
+
+        //        HttpContext.Session.Set("products", lstBillDetailView);
+        //    }
+
+        //    return RedirectToAction("Index");
+        //}
+
+        [HttpPost]
+        [Route("/Cart/Increase")]
+        public JsonResult Increase(string productId)
+        {
+            List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
+            if (lstBillDetailView == null)
+            {
+                lstBillDetailView = new List<BillDetailViewModels>();
+            } 
+
+            int.TryParse(productId, out int id);
+            var product = lstBillDetailView.FirstOrDefault(p => p.ProductID == id);
+
+            if (lstBillDetailView.Any(p => p.ProductID == id))
+            {
+                decimal productPrice = product.Price;
+                int updateQuantity = product.Quantity;
+
+                if (updateQuantity >= 20)
+                {
+                    updateQuantity = product.Quantity;
+                }
+                else
+                {
+                    updateQuantity = product.Quantity += 1;
+                }
+
+                decimal totalPrice = product.Total = updateQuantity * productPrice;
+
+                product.Quantity = updateQuantity;
+                product.Total = totalPrice;
+
+                HttpContext.Session.Set("products", lstBillDetailView);
+            }
+
+            return Json(lstBillDetailView);
+        }
+
+        [HttpPost]
+        [Route("/Cart/Decrease")]
+
+        public IActionResult Decrease(string productId)
+        {
+            List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
+            if (lstBillDetailView == null)
+            {
+                lstBillDetailView = new List<BillDetailViewModels>();
+            } 
+
+            int.TryParse(productId, out int id);
+            var product = lstBillDetailView.FirstOrDefault(p => p.ProductID == id);
+
+            if (lstBillDetailView.Any(p => p.ProductID == id))
+            {
+                decimal productPrice = product.Price;
+                int updateQuantity = product.Quantity;
+
+                if (updateQuantity <= 0)
+                {
+                    updateQuantity = product.Quantity;
+                }
+                else
+                {
+                    updateQuantity = product.Quantity -= 1;
+                }
+
+                decimal totalPrice = product.Total = updateQuantity * productPrice;
+
+                product.Quantity = updateQuantity;
+                product.Total = totalPrice;
+
+                HttpContext.Session.Set("products", lstBillDetailView);
+            }
+
+            return Json(lstBillDetailView);
         }
 
         [HttpPost]
