4e2b227d0f56cdb40b0c9df206288892d43b7f03
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2023-04-15 01:57:58 +0700

    -fix bug: image profile doesn't exist properly -cmt admin UI which doesn't use

--- WebApplication1/Controllers/CartController.cs
+++ WebApplication1/Controllers/CartController.cs
@@ -33,7 +33,7 @@ namespace WebApplication1.Controllers
 
             foreach (var item in lstBillDetailView)
             {
-                if(item.Quantity != 0)
+                if (item.Quantity != 0)
                 {
                     finalLst.Add(item);
                 }
@@ -41,217 +41,6 @@ namespace WebApplication1.Controllers
 
             return View(finalLst);
         }
-
-        //public IActionResult Increase(int id)
-        //{
-        //    List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
-        //    if (lstBillDetailView == null)
-        //    {
-        //        lstBillDetailView = new List<BillDetailViewModels>();
-        //    }
-
-        //    var product = lstBillDetailView.FirstOrDefault(p => p.ProductID == id);
-
-        //    if (lstBillDetailView.Any(p => p.ProductID == id))
-        //    {
-
-        //        if (lstBillDetailView.Count() > 19 || lstBillDetailView.Sum(b => b.Total) > 50)
-        //        {
-        //            TempData["cartFlag"] = "Cart reached limitation";
-        //            RedirectToAction("Index", "Category");
-        //        }
-        //        else
-        //        {
-        //            if (product.Quantity > 4)
-        //            {
-        //                product.Quantity = product.Quantity;
-        //                HttpContext.Session.Set("products", lstBillDetailView);
-        //            }
-        //            else
-        //            {
-        //                product.Quantity += 1;
-        //                product.Total = product.Quantity * product.Price;
-
-        //            }
-        //        }
-        //        product.Total = product.Quantity * product.Price;
-
-        //        HttpContext.Session.Set("products", lstBillDetailView);
-        //    }
-
-        //    return RedirectToAction("Index", "Cart");
-
-        //}
-
-        //public IActionResult Decrease(int id)
-        //{
-        //    List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
-        //    if (lstBillDetailView == null)
-        //    {
-        //        lstBillDetailView = new List<BillDetailViewModels>();
-        //    }
-
-        //    var productCart = lstBillDetailView.FirstOrDefault(p => p.ProductID == id);
-
-        //    if (lstBillDetailView.Any(p => p.ProductID == id))
-        //    {
-        //        if (lstBillDetailView.Count() == 0 || productCart.Quantity == 0)
-        //        {
-        //            productCart.Quantity = 0;
-        //            HttpContext.Session.Set("products", lstBillDetailView);
-        //        }
-        //        else
-        //        {
-        //            if (productCart.Quantity == 0)
-        //            {
-        //                lstBillDetailView.Remove(productCart);
-        //                HttpContext.Session.Set("products", lstBillDetailView);
-        //            }
-        //            productCart.Quantity -= 1;
-        //            HttpContext.Session.Set("products", lstBillDetailView);
-        //        }
-
-        //    }
-
-        //    return RedirectToAction("Index", "Cart");
-        //}
-
-        public IActionResult Remove(int id)
-        {
-            List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
-            if (lstBillDetailView == null)
-            {
-                lstBillDetailView = new List<BillDetailViewModels>();
-            }
-
-            var product = lstBillDetailView.Find(p => p.ProductID == id);
-
-            if (lstBillDetailView.Any(p => p.ProductID == id))
-            {
-                lstBillDetailView.Remove(product);
-                HttpContext.Session.Set("products", lstBillDetailView);
-            }
-
-            if (lstBillDetailView.Sum(p => p.Quantity) == 0)
-            {
-                TempData["cartEmpty"] = "Cart is empty. Please choose at least one product";
-                lstBillDetailView = new List<BillDetailViewModels>();
-
-                HttpContext.Session.Set("products", lstBillDetailView);
-
-                return RedirectToAction("Index", "Category");
-            }
-            return RedirectToAction("Index", "Cart");
-        }
-
-        [HttpPost]
-        public IActionResult CheckOut(string stripeEmail, string stripeToken, decimal total)
-        {
-            List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
-            if (lstBillDetailView == null)
-            {
-                lstBillDetailView = new List<BillDetailViewModels>();
-            }
-
-            string email = "";
-            int id = 0;
-            Models.entities.Account accBill = new Models.entities.Account();
-
-            WebApplication1.Models.entities.Account acc = HttpContext.Session.Get<WebApplication1.Models.entities.Account>("acc");
-            if (acc == null)
-            {
-                acc = new Models.entities.Account();
-                HttpContext.Session.Set("acc", acc);
-            }
-            Models.entities.Account account = HttpContext.Session.Get<WebApplication1.Models.entities.Account>("account");
-            if (account == null)
-            {
-                account = new Models.entities.Account();
-                HttpContext.Session.Set("account", account);
-            }
-
-            if (acc.Email.Equals("") && account.Email.Equals(""))
-            {
-                TempData["checkOutFlag"] = "Card is valid but please log in before payment";
-                return RedirectToAction("Index", "Account");
-            }
-            else
-            {
-                if (account.Email.Equals(""))
-                {
-                    email = acc.Email;
-                    accBill = acc;
-                    id = acc.ID;
-                }
-                else
-                {
-                    email = account.Email;
-                    accBill = account;
-                    id = account.ID;
-                }
-            }
-            var customers = new CustomerService();
-            var charges = new ChargeService();
-
-            var customer = customers.Create(new CustomerCreateOptions
-            {
-                Email = email,
-                Source = stripeToken
-            });
-
-            var charge = charges.Create(new ChargeCreateOptions
-            {
-                Amount = long.Parse(lstBillDetailView.Sum(p => p.Total).ToString()) * 100,
-                Description = "Test Payment",
-                Currency = "usd",
-                Customer = customer.Id,
-                ReceiptEmail = stripeEmail,
-                Metadata = new Dictionary<string, string>
-            {
-                {"OrderID", "1"},
-                {"ReceiptNo", "1"}
-            }
-            });
-
-            if (charge.Status.Equals("succeeded"))
-            {
-                string BalanceTransactionID = charge.BalanceTransactionId;
-                billDetail = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
-                Bill bill = new Bill
-                {
-                    CreatedTime = DateTime.Now.ToString(),
-                    Total = total,
-                    Status = (int)EnumStatus.Paid,
-                    PaymentId = (int)EnumStatus.Stripe,
-                    PaymentCode = BalanceTransactionID,
-                    AccId = id
-                };
-
-                FacadeMaker.Instance.CreateBill(bill);
-
-
-                List<Bill> billView = _context.Bills.Where(b => b.AccId == accBill.ID).ToList();
-                List<BillViewModels> lstBillVM = new List<BillViewModels>();
-                foreach (var item in billView)
-                {
-                    BillViewModels billVM = new BillViewModels(item);
-                    lstBillVM.Add(billVM);
-                }
-                HttpContext.Session.Set("bill", lstBillVM);
-
-                return RedirectToAction("Success", "Home");
-            }
-            else
-            {
-                return RedirectToAction("Fail", "Home");
-            }
-
-        }
-
-        //public IActionResult CheckOut()
-        //{
-        //    return View();
-        //}
 
         [HttpPost]
         public JsonResult Increase(int id, decimal price)
@@ -324,7 +113,7 @@ namespace WebApplication1.Controllers
 
             foreach (var item in lstBillDetailView)
             {
-                if(item.Quantity != 0)
+                if (item.Quantity != 0)
                 {
                     finalLst.Add(item);
                 }
@@ -334,5 +123,131 @@ namespace WebApplication1.Controllers
 
             return Json(HttpContext.Session.Get<List<BillDetailViewModels>>("products"));
         }
+
+
+        public IActionResult Remove(int id)
+        {
+            List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
+            if (lstBillDetailView == null)
+            {
+                lstBillDetailView = new List<BillDetailViewModels>();
+            }
+
+            var product = lstBillDetailView.Find(p => p.ProductID == id);
+
+            if (lstBillDetailView.Any(p => p.ProductID == id))
+            {
+                lstBillDetailView.Remove(product);
+                HttpContext.Session.Set("products", lstBillDetailView);
+            }
+
+            if (lstBillDetailView.Sum(p => p.Quantity) == 0)
+            {
+                TempData["cartEmpty"] = "Cart is empty. Please choose at least one product";
+                lstBillDetailView = new List<BillDetailViewModels>();
+
+                HttpContext.Session.Set("products", lstBillDetailView);
+
+                return RedirectToAction("Index", "Category");
+            }
+            return RedirectToAction("Index", "Cart");
+        }
+
+        [HttpPost]
+        public IActionResult CheckOut(string stripeEmail, string stripeToken, decimal total)
+        {
+            List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
+            if (lstBillDetailView == null)
+            {
+                lstBillDetailView = new List<BillDetailViewModels>();
+            }
+
+            string email = "";
+            int id = 0;
+            Models.entities.Account accBill = new Models.entities.Account();
+
+            Models.entities.Account account = HttpContext.Session.Get<WebApplication1.Models.entities.Account>("account");
+            if (account == null)
+            {
+                account = new Models.entities.Account();
+                HttpContext.Session.Set("account", account);
+            }
+
+            if (account.Email.Equals(""))
+            {
+                TempData["checkOutFlag"] = "Card is valid but please log in before payment";
+                return RedirectToAction("Index", "Account");
+            }
+            else
+            {
+                email = account.Email;
+                accBill = account;
+                id = account.ID;
+            }
+
+            var customers = new CustomerService();
+            var charges = new ChargeService();
+
+            var customer = customers.Create(new CustomerCreateOptions
+            {
+                Email = email,
+                Source = stripeToken
+            });
+
+            var charge = charges.Create(new ChargeCreateOptions
+            {
+                Amount = long.Parse(lstBillDetailView.Sum(p => p.Total).ToString()) * 100,
+                Description = "Test Payment",
+                Currency = "usd",
+                Customer = customer.Id,
+                ReceiptEmail = stripeEmail,
+                Metadata = new Dictionary<string, string>
+            {
+                {"OrderID", "1"},
+                {"ReceiptNo", "1"}
+            }
+            });
+
+            if (charge.Status.Equals("succeeded"))
+            {
+                string BalanceTransactionID = charge.BalanceTransactionId;
+                billDetail = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
+                Bill bill = new Bill
+                {
+                    CreatedTime = DateTime.Now.ToString(),
+                    Total = total,
+                    Status = (int)EnumStatus.Paid,
+                    PaymentId = (int)EnumStatus.Stripe,
+                    PaymentCode = BalanceTransactionID,
+                    AccId = id
+                };
+
+                FacadeMaker.Instance.CreateBill(bill);
+
+
+                List<Bill> billView = _context.Bills.Where(b => b.AccId == accBill.ID).ToList();
+                List<BillViewModels> lstBillVM = new List<BillViewModels>();
+                foreach (var item in billView)
+                {
+                    BillViewModels billVM = new BillViewModels(item);
+                    lstBillVM.Add(billVM);
+                }
+                HttpContext.Session.Set("bill", lstBillVM);
+
+                return RedirectToAction("Success", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Fail", "Home");
+            }
+
+        }
+
+        //public IActionResult CheckOut()
+        //{
+        //    return View();
+        //}
+
+
     }
 }
--- WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/Controllers/CategoryController.cs
@@ -24,56 +24,6 @@ namespace WebApplication1.Controllers
             return View(productView);
         }
 
-        //[HttpPost]
-        //public IActionResult AddToCart(int id)
-        //{
-        //    List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
-        //    if (lstBillDetailView == null)
-        //    {
-        //        lstBillDetailView = new List<BillDetailViewModels>();
-        //    }
-        //    var productCart = lstBillDetailView.FirstOrDefault(p => p.ProductID == id);
-
-        //    Product product = FacadeMaker.Instance.GetProductById(id);
-        //    ProductViewModels productView = new ProductViewModels(product);
-
-        //    BillDetailViewModels billDetailView = new BillDetailViewModels();
-
-
-        //    if (lstBillDetailView.Any(p => p.ProductID == id))
-        //    {
-        //        if (lstBillDetailView.Count() > 19 || lstBillDetailView.Sum(b => b.Total) > 50)
-        //        {
-        //            TempData["cartFlag"] = "Cart reached limitation";
-        //            RedirectToAction("Index", "Category");
-        //        }
-        //        else
-        //        {
-        //            if (productCart.Quantity > 4)
-        //            {
-        //                productCart.Quantity = productCart.Quantity;
-        //                HttpContext.Session.Set("products", lstBillDetailView);
-        //            }
-        //            productCart.Quantity += 1;
-        //            productCart.Total = productCart.Quantity * productView.Price;
-        //            HttpContext.Session.Set("products", lstBillDetailView);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        billDetailView.ProductID = id;
-        //        billDetailView.ProductName = product.Name;
-        //        billDetailView.ProductImage = product.Image;
-        //        billDetailView.Price = product.Price;
-        //        billDetailView.Quantity = 1;
-        //        billDetailView.Total = billDetailView.Price * billDetailView.Quantity;
-        //        lstBillDetailView.Add(billDetailView);
-        //        HttpContext.Session.Set("products", lstBillDetailView);
-
-        //    }
-
-        //    return RedirectToAction(nameof(Index));
-        //}
         [HttpPost]
         public JsonResult AddToCart(int id, decimal price)
         {
@@ -238,38 +188,6 @@ namespace WebApplication1.Controllers
             return Json(HttpContext.Session.Get<List<BillDetailViewModels>>("products"));
         }
 
-        //[HttpPost]
-        //public IActionResult RemoveFromCart(int id)
-        //{
-        //    List<BillDetailViewModels> lstBillDetailView = HttpContext.Session.Get<List<BillDetailViewModels>>("products");
-        //    if (lstBillDetailView == null)
-        //    {
-        //        lstBillDetailView = new List<BillDetailViewModels>();
-        //    }
-
-        //    var productCart = lstBillDetailView.FirstOrDefault(p => p.ProductID == id);
-
-        //    if (lstBillDetailView.Any(p => p.ProductID == id))
-        //    {
-        //        if (lstBillDetailView.Count() == 0 || productCart.Quantity == 0)
-        //        {
-        //            productCart.Quantity = 0;
-        //            HttpContext.Session.Set("products", lstBillDetailView);
-        //        }
-        //        else
-        //        {
-        //            if (productCart.Quantity == 0)
-        //            {
-        //                lstBillDetailView.Remove(productCart);
-        //                HttpContext.Session.Set("products", lstBillDetailView);
-        //            }
-        //            productCart.Quantity -= 1;
-        //            HttpContext.Session.Set("products", lstBillDetailView);
-        //        }
-
-        //    }
-
-        //    return RedirectToAction(nameof(Index));
-        //}
+       
     }
 }
